f59e3099c49127115ff4e89ddab49457e0a918f0
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2024-10-07 20:16:15 -0300

    style: adjusting header to dark theme

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -48,12 +48,12 @@ export function Header() {
         className=" relative
       flex justify-between items-center 
       py-2 px-6  max-w-5xl mx-auto
-      bg-black/50 text-white dark:bg-white-25 backdrop-blur-[2px]
-      border rounded-xl border-black/30 dark:border-dark-header-bg transition-all duration-300
+      bg-black/50 text-white dark:text-dark-neutral-1 dark:bg-white/25 backdrop-blur-[2px]
+      border rounded-xl border-black/30 dark:border-bg-white/10 transition-all duration-300
       z-20"
       >
         <nav>
-          <ul className="flex gap-10 text-white font-semibold">
+          <ul className="flex gap-10 text-white font-semibold dark:">
             {menuOptions.map((option) => (
               <motion.li whileHover={{ fontSize: "20px" }} key={option.name}>
                 <a href={option.href}>{option.name}</a>
@@ -62,7 +62,7 @@ export function Header() {
           </ul>
         </nav>
 
-        <nav>
+        <nav className="text-white">
           <ul className="flex gap-6">
             <motion.li whileHover={{ scale: 1.1 }}>
               <a href="">
